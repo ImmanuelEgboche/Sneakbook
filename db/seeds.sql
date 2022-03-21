810634b4e5adb07e63e75c10d537ad6aa78ccc61
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-03-21 19:00:03 +0000

    added email row

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS info;
 CREATE TABLE info (
     id serial PRIMARY KEY,
     username varchar(255) NOT NULL,
-    password varchar(255) NOT NULL
+    password varchar(255) NOT NULL,
+    email varchar(255) NOT NULL
 );
